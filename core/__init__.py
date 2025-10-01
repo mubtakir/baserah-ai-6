193aaeb14a9dd0aded73fffdfe365d7c12c9ea1c
mubtakir <155906145+mubtakir@users.noreply.github.com>
2025-10-01 21:43:35 +0300

    رفع مشروع بصيرة (بدون ملفات git): core/__init__.py

---
+++ core/__init__.py
@@ -0,0 +1,34 @@
+"""
+🧬 النواة الأساسية لنظام بصيرة الثوري
+Core Components of Revolutionary Basera System
+
+المطور: باسل يحيى عبدالله
+جميع الأفكار والنظريات من إبداع باسل يحيى عبدالله
+
+هذا المجلد يحتوي على المكونات الأساسية:
+- المعادلة الأم الثورية
+- النواة التفكيرية متعددة الطبقات  
+- نظام الخبير/المستكشف
+- المعادلات التكيفية
+- معادلة الشكل العام
+- مكتبة المعادلات الثورية
+"""
+
+from .revolutionary_mother_equation import RevolutionaryMotherEquation
+from .complete_multi_layer_thinking_core import CompleteMultiLayerThinkingCore
+from .expert_explorer_system import ExpertExplorerSystem
+from .adaptive_revolutionary_equations_fixed import AdaptiveRevolutionaryEquation
+from .enhanced_general_shape_equation import EnhancedGeneralShapeEquation
+from .revolutionary_equation_library import RevolutionaryEquationLibrary
+
+__all__ = [
+    'RevolutionaryMotherEquation',
+    'CompleteMultiLayerThinkingCore', 
+    'ExpertExplorerSystem',
+    'AdaptiveRevolutionaryEquation',
+    'EnhancedGeneralShapeEquation',
+    'RevolutionaryEquationLibrary'
+]
+
+__version__ = "1.0.0"
+__author__ = "باسل يحيى عبدالله"
